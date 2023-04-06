16bd1e27bb90eceb818dd5f875581e1a73cb1c2c
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-04-06 15:07:01 -0300

    feat: typeit component

---
+++ src/components/Title.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import TypeIt from 'typeit-react'
+
+export default function Title() {
+  return (
+    <h1 className="font-title text-brand-purple text-5xl bg-brand-black w-fit px-4 py-2 bg-opacity-80 rounded-md">
+      <TypeIt
+        options={{
+          speed: 300,
+          waitUntilVisible: true,
+        }}
+        getBeforeInit={(instance) => {
+          instance.type('Hype $').pause(500).delete(1).pause(500).type('Store')
+
+          return instance
+        }}
+      />
+    </h1>
+  )
+}
