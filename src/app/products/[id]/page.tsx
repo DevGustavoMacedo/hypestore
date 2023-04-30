df5856642d25f4f08c5d97fe0fa96e0574e3ce99
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-04-29 21:04:38 -0300

    feat: products page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,34 +1,35 @@
-import Footer from '@/components/Footer'
+import GoBack from '@/components/GoBack'
 import ProductCard from '@/components/ProductCard'
-import TParams from '@/types/Params'
-import TProduct from '@/types/Product'
-import Image from 'next/image'
-import Link from 'next/link'
+import TProduct from '@/types/TProduct'
 
 export async function generateStaticParams() {
-  const products: TProduct[] = await fetch('https://fakestoreapi.com/products').then((res) =>
-    res.json()
-  )
+  const products: TProduct[] = await fetch(
+    'https://fakestoreapi.com/products',
+  ).then((res) => res.json())
 
-  const idProducts = products.filter((item) => item.id)
+  const idProducts = products.map((item) => item.id)
 
   return idProducts
 }
 
-export default function Product({ params }: TParams) {
+type Props = {
+  params: {
+    category: string
+  }
+}
+
+export default function Product({ params }: Props) {
   return (
-    <section className="bg-brand-black">
-      <div className="grid grid-cols-3 items-center">
-        <Link href="/products" className="justify-self-center" passHref>
-          <Image width={50} height={50} alt="Go back" src="/back.svg" className="w-6 h-auto" />
-        </Link>
-        <h2 className="font-title text-brand-purple text-2xl uppercase bg-brand-black w-fit py-4 mx-auto">
+    <section className="dark:bg-brand-black bg-brand-white">
+            <div className="flex items-center justify-around">
+        <GoBack />
+        <h2 className="font-title text-brand-purple text-xl uppercase dark:bg-brand-black bg-brand-white py-5">
           Product
         </h2>
+        <div></div>
       </div>
       {/* @ts-expect-error Async Server Component */}
       <ProductCard id={params.id} />
-      <Footer />
     </section>
   )
 }
