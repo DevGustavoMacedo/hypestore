c49fdee2431cf0d6830b73f41806debbb00dba57
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-05-03 09:20:11 -0300

    feat: product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import TProduct from '@/types/Product'
+import TProduct from '@/types/TProduct'
 import Image from 'next/image'
 
 export default async function ProductCard({ id }: { id: number }) {
@@ -15,23 +15,44 @@ export default async function ProductCard({ id }: { id: number }) {
   }
 
   return (
-    <div className="mx-auto w-4/5 rounded-xl rounded-s-none p-2 bg-brand-purple">
+    <div
+      className="mx-auto w-4/5 rounded-xl rounded-s-none p-2 bg-brand-purple
+    sm:w-1/2 lg:flex lg:w-4/5 lg:gap-2
+    "
+    >
       <Image
         src={product.image}
         height={600}
         width={300}
         alt={product.title}
         priority={true}
-        className="rounded h-auto w-full object-cover"
+        className="rounded h-auto w-full object-cover
+        lg:w-1/2"
       />
-      <div className="flex flex-col p-4 gap-4 text-gray-200 bg-brand-black font-text">
-        <h3 className="font-title text-lg text-brand-purple">
+      <div
+        className="flex flex-col p-4 gap-4 dark:text-brand-white text-brand-black bg-brand-white dark:bg-brand-black font-text
+      lg:p-8 lg:gap-8"
+      >
+        <h3
+          className="font-text font-bold text-lg text-brand-purple text-center
+        lg:text-2xl"
+        >
           {product.title}
         </h3>
-        <p className="border-brand-purple border-2 text-sm rounded border-dashed py-2 px-3 text-justify h-28 overflow-x-auto">
+        <p
+          className="border-brand-purple border-2 text-sm rounded border-dashed py-2 px-3 text-justify h-28 overflow-x-auto
+        sm:text-base lg:text-xl lg:p-4 lg:h-36
+        "
+        >
           {product.description}
         </p>
-        <p className="text-xs">Category: {product.category}</p>
+        <p
+          className="text-xs 
+        sm:text-sm lg:text-lg"
+        >
+          <span className="text-brand-purple font-bold">Category: </span>{' '}
+          {product.category}
+        </p>
         <p className="flex justify-between items-center">
           <span className="flex">
             {stars.map((_, index) => (
@@ -41,13 +62,22 @@ export default async function ProductCard({ id }: { id: number }) {
                 height={40}
                 alt=""
                 src="/star.svg"
-                className="fill-brand-purple w-4 h-auto"
+                className="fill-brand-purple w-4 h-auto
+                lg:w-8"
               />
             ))}
-            <span className="text-xs">{product.rating.count}</span>
+            <span
+              className="text-xs 
+            sm:text-sm lg:text-lg"
+            >
+              {product.rating.count}
+            </span>
           </span>
-          <span className="text-brand-purple font-title text-2xl">
-            U$ {product.price}
+          <span
+            className="dark:text-brand-white text-brand-black font-title text-2xl
+          lg:text-4xl 2xl:text-5xl"
+          >
+            <span className="text-brand-purple">U$ </span> {product.price}
           </span>
         </p>
       </div>
