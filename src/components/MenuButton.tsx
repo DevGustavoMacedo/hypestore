0458b97dde9587c2fb5b17a3e8c56a101e142ebe
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-05-04 10:40:06 -0300

    feat: menu button

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -5,20 +5,14 @@ export default function MenuButton() {
     const menu = document.getElementById('menu')
     const btnMenu = document.getElementById('btnMenu')
 
-    const toggleClasses = (el: HTMLElement, ...cls: string[]) =>
-      cls.map((cl: string) => el.classList.toggle(cl))
+    if (menu && btnMenu) {
+      menu.classList.toggle('opacity-0')
+      menu.classList.toggle('pointer-events-none')
+      menu.classList.toggle('animate-open-menu')
 
-    if (menu) {
-      toggleClasses(
-        menu,
-        'opacity-0',
-        'pointer-events-none',
-        'animate-open-menu',
-      )
-    }
+      btnMenu.classList.toggle('toggle-btn')
 
-    if (btnMenu) {
-      toggleClasses(btnMenu, 'toggle-btn')
+      document.body.classList.toggle('overflow-hidden')
     }
   }
 
@@ -26,12 +20,15 @@ export default function MenuButton() {
     <div
       id="btnMenu"
       onClick={toggleMenu}
-      className="sm:hidden relative h-9 w-12 z-30 cursor-pointer bg-brand-darkblue rounded"
+      className="relative h-8 w-8 z-30 cursor-pointer rounded bg-brand-purple
+      md:h-10 md:w-10 lg:h-12 lg:w-12"
     >
       <button
         className="
-      before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-2 before:rounded before:bg-brand-white before:transition-all before:duration-500 
-      after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-1 after:rounded after:bg-brand-white after:transition-all after:duration-500 
+      before:absolute before:h-1 before:w-8 before:translate-x-0 before:-translate-y-4 before:rounded before:bg-brand-white dark:before:bg-brand-black before:transition-all before:duration-500 
+      after:absolute after:h-1 after:w-8 after:translate-x-0 after:translate-y-0 after:rounded dark:after:bg-brand-black after:bg-brand-white after:transition-all after:duration-500 
+      md:before:w-10 md:after:w-10
+      lg:before:w-12 lg:after:w-12 lg:before:h-2 lg:after:h-2 
       "
       />
     </div>
